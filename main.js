5ffc2bc9bb8cef6d3730836d0078c6b7be571df3
bima-taruna <bimataruna21@gmail.com>
2024-09-06 21:43:56 +0700

    create function to map weather data from api

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-import { icon } from "@fortawesome/fontawesome-svg-core";
 import "./style.css";
 import { searchIcon } from "./module/icon";
 (function () {
   const weatherForm = document.querySelector(".location-form");
   const locationInput = document.getElementById("weather-location");
   const submitButton = document.querySelector("button");
+  const weatherStat = document.querySelector(".weather-stat");
   submitButton.innerHTML = searchIcon;
 
   weatherForm.addEventListener("submit", (e) => {
     e.preventDefault();
     fetchWeather(locationInput.value).then((data) => {
-      console.log(data);
-      // let conditionPanels = conditionPanel({day:dat})
+      const weatherData = mapWeatherData(data);
+      console.log(weatherData);
     });
   });
 
@@ -37,5 +37,17 @@ import { searchIcon } from "./module/icon";
       loading = false;
       return error;
     }
+  }
+
+  function mapWeatherData(data) {
+    const dataMap = new Map();
+    dataMap.set("address", data.resolvedAddress);
+    dataMap.set("description", data.description);
+    dataMap.set("condition", data.currentConditions.conditions);
+    dataMap.set("icon", data.currentConditions.icon);
+    dataMap.set("temp", data.currentConditions.temp);
+    dataMap.set("humidity", data.currentConditions.humidity);
+    dataMap.set("windSpeed", data.currentConditions.windspeed);
+    return dataMap;
   }
 })();
