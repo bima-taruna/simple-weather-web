fb64d605f155cd2bdcf1ab6e408222941200d656
bima-taruna <bimataruna21@gmail.com>
2024-09-07 09:15:00 +0700

    use hashtable to map instead of map, and create function to render weather stat component after button click

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import "./style.css";
 import { searchIcon } from "./module/icon";
+import weatherStatComp from "./weatherStat";
 (function () {
   const weatherForm = document.querySelector(".location-form");
   const locationInput = document.getElementById("weather-location");
   const submitButton = document.querySelector("button");
-  const weatherStat = document.querySelector(".weather-stat");
+  const weatherStatContainer = document.querySelector(".weather-stat");
   submitButton.innerHTML = searchIcon;
 
   weatherForm.addEventListener("submit", (e) => {
@@ -12,6 +13,7 @@ import { searchIcon } from "./module/icon";
     fetchWeather(locationInput.value).then((data) => {
       const weatherData = mapWeatherData(data);
       console.log(weatherData);
+      renderStatPanel(weatherData);
     });
   });
 
@@ -39,15 +41,37 @@ import { searchIcon } from "./module/icon";
     }
   }
 
+  /* The `mapWeatherData` function is taking a `data` object as input, which likely contains weather
+  information fetched from an API. then put it in map, Inside the function: */
   function mapWeatherData(data) {
-    const dataMap = new Map();
-    dataMap.set("address", data.resolvedAddress);
-    dataMap.set("description", data.description);
-    dataMap.set("condition", data.currentConditions.conditions);
-    dataMap.set("icon", data.currentConditions.icon);
-    dataMap.set("temp", data.currentConditions.temp);
-    dataMap.set("humidity", data.currentConditions.humidity);
-    dataMap.set("windSpeed", data.currentConditions.windspeed);
-    return dataMap;
+    const dataTable = {
+      address: data.resolvedAddress,
+      description: data.description,
+      condition: data.currentConditions.conditions,
+      icon: data.currentConditions.icon,
+      temp: data.currentConditions.temp,
+      humidity: data.currentConditions.humidity,
+      windSpeed: data.currentConditions.windspeed,
+    };
+    return dataTable;
+  }
+
+  function renderStatPanel(params) {
+    const weatherStat = weatherStatComp();
+    weatherStat.render({
+      address: params.address,
+      description: params.description,
+      condition: params.condition,
+      icon: params.icon,
+      temp: params.temp,
+      humidity: params.humidity,
+      windSpeed: params.windSpeed,
+    });
+    while (weatherStatContainer.children.length > 0) {
+      weatherStatContainer.textContent = "";
+    }
+    weatherStatContainer.appendChild(weatherStat.descriptionPanel);
+    weatherStatContainer.appendChild(weatherStat.iconPanel);
+    weatherStatContainer.appendChild(weatherStat.statPanel);
   }
 })();
