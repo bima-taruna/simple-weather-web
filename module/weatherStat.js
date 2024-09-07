2fda5e87331f5b020f398b036c7eb606926ae2ed
bima-taruna <bimataruna21@gmail.com>
2024-09-07 10:31:41 +0700

    implement wind and droplet icon

--- module/weatherStat.js
+++ module/weatherStat.js
@@ -1,7 +1,13 @@
+import { dropletIcon, windIcon } from "./icon";
+
 function weatherStatComp() {
   const iconPanel = document.createElement("img");
   const statPanel = document.createElement("section");
   const descriptionPanel = document.createElement("desc");
+  const humidityIcon = document.createElement("span");
+  const windSpeedIcon = document.createElement("span");
+  humidityIcon.innerHTML = dropletIcon;
+  windSpeedIcon.innerHTML = windIcon;
   statPanel.classList.add("stat-panel");
   descriptionPanel.classList.add("desc-stat");
   function render({
@@ -14,18 +20,22 @@ function weatherStatComp() {
     windSpeed,
   }) {
     iconPanel.src = `./assets/icon/${icon}.png`;
-    console.log(iconPanel.src);
     descriptionPanel.textContent = `${description}`;
     statPanel.innerHTML = `
-        <div class="temp-stat">${temp}</div>
+        <div class="temp-stat">${temp}°C</div>
         <div class="cond-stat">${condition}</div>
         <div class="address-stat">${address}</div>
         <div class="other-stat">
-            <div class="humid-stat">${humidity}</div>
-            <div class="wind-stat">${windSpeed}</div>
+            <div class="humid-stat">
+            ${humidity}%</div>
+            <div class="wind-stat">${windSpeed} Km/h</div>
         </div>
 
     `;
+    const humidStat = statPanel.querySelector(".humid-stat");
+    const windStat = statPanel.querySelector(".wind-stat");
+    humidStat.prepend(humidityIcon);
+    windStat.prepend(windSpeedIcon);
   }
 
   return {
