de350d1e329ee0595fc0cd27b5f8e55a8bcb8aa7
bima-taruna <bimataruna21@gmail.com>
2024-09-07 10:31:16 +0700

    add wind and droplet icon

--- module/icon.js
+++ module/icon.js
@@ -1,9 +1,22 @@
 import { library, icon } from "@fortawesome/fontawesome-svg-core";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faDroplet } from "@fortawesome/free-solid-svg-icons";
+import { faWind } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faMagnifyingGlass);
-
+library.add(faDroplet);
+library.add(faWind);
 export const searchIcon = icon({
   prefix: "fas",
   iconName: "magnifying-glass",
+}).html;
+
+export const dropletIcon = icon({
+  prefix: "fas",
+  iconName: "droplet",
+}).html;
+
+export const windIcon = icon({
+  prefix: "fas",
+  iconName: "wind",
 }).html;
